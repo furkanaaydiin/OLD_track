f18b3954aa62303255adb231669e1bce40d39105
alperen.karal <58168612+Dexaben@users.noreply.github.com>
2022-09-05 23:15:42 +0300

    enemy path system

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using DG.Tweening;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private CinemachinePath cinemachinePath;
+    private List<Vector3> positions = new();
+    private int _currentPositionIndex = -1;
+    private bool _isDead = false;
+    private static readonly int IsRun = Animator.StringToHash("IsRun");
+    private static readonly int IsDeath = Animator.StringToHash("IsDead");
+
+    void Start()
+    {
+        for (var i = 0; i < cinemachinePath.m_Waypoints.Length; i++)
+        {
+            positions.Add(cinemachinePath.m_Waypoints[i].position+cinemachinePath.transform.position);
+        }
+        NextPosition();
+    }
+    private void Respawn()
+    {
+        _isDead = false;
+        Transform playerTransform;
+        (playerTransform = transform).DOKill();
+        playerTransform.position = positions[_currentPositionIndex-1];
+        NextPosition(true);
+    }
+
+    private IEnumerator Dead()
+    {
+        _isDead = true;
+        transform.DOKill();
+        animator.SetTrigger(IsDeath);
+        yield return new WaitForSeconds(5);
+        Respawn();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("resistance"))
+        {
+            if (_isDead)
+                return;
+            StartCoroutine(Dead());
+        }
+    }
+
+    private void NextPosition(bool withoutIncrease = false)
+    {
+        if(!withoutIncrease)
+            _currentPositionIndex++;
+        if(!_isDead)
+            animator.SetTrigger(IsRun);
+        if (_currentPositionIndex >= positions.Count)
+        {
+            Finished();
+            return;
+        }
+
+        transform.DOKill();
+        transform.DOMove(positions[_currentPositionIndex],
+            Vector3.Distance(transform.position, positions[_currentPositionIndex])).SetEase(Ease.Linear).OnComplete(()=>NextPosition());
+    }
+
+    private void Finished()
+    {
+        transform.DOKill();
+    }
+}
